945910f71e62d483515b592a9209f740a30844b0
Jaivardhan Shukla <93859359+jaivsh@users.noreply.github.com>
2023-06-21 03:56:31 +0530

    Work completed

--- app/src/main/java/com/parsanatech/crazycoder/SplashScreenActivity.kt
+++ app/src/main/java/com/parsanatech/crazycoder/SplashScreenActivity.kt
@@ -1,6 +1,7 @@
 package com.parsanatech.crazycoder
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
@@ -8,9 +9,16 @@ import java.text.DecimalFormat
 import java.text.NumberFormat
 
 class SplashScreenActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val sharedpref: SharedPreferences =
+            applicationContext.getSharedPreferences(
+                "com.itssuryansh.taaveez",
+                MODE_PRIVATE
+            )
 
         val interval:Long=2000
 
@@ -20,10 +28,22 @@ class SplashScreenActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-                startActivity(Intent(this@SplashScreenActivity, SignUp::class.java))
+               // startActivity(Intent(this@SplashScreenActivity, SignUp::class.java))
                 finish()
             }
         }.start()
+
+        val token: String? = sharedpref.getString("token", null)
+        if (token == "false" || token == null) {
+            val mv = Intent(this, MyCustomAppIntro::class.java)
+            startActivity(mv)
+
+                  sharedpref.edit().putString("token", "true").apply()
+        } else {
+            sharedpref.edit().putString("token", "false").apply()
+            startActivity(Intent(this@SplashScreenActivity, MyCustomAppIntro::class.java))
+
+        }
 
 
     }
